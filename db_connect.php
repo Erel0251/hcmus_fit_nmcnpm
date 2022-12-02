b233c735680614f2ebf29b7a812b8d8954621c3d
Erel <tranducanhghu@gmail.com>
2022-12-02 08:58:24 +0700

    connect database

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cantin";
+try {
+    $conn = new PDO("mysql:host=$server; dbname=$dbname", $username, $password);
+    // Thiết lập lỗi của PDO cho ngoại lệ
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Kết nối thành công";
+} catch (PDOException $e){
+    echo "Lỗi kết nối: " . $e->getMessage();
+}
+?>
