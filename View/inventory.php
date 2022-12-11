09bc3973151d35345a37aa2d7157529b3b83fe5d
Erel <tranducanhghu@gmail.com>
2022-12-12 06:01:27 +0700

    inventory page

--- View/inventory.php
+++ View/inventory.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+include "../Model/db_connect.php";
+
+
+$sql = "SELECT hang.TENHANG, phieu.NGAYNHAP, ct.SOLUONG, hang.DONGIA, phieu.DIACHIKHO 
+        FROM ct_nhap ct 
+        JOIN mat_hang hang on ct.MAHANG = hang.MAHANG
+        JOIN phieu_nhap_hang phieu on ct.MAPHIEUNHAP = phieu.MAPHIEUNHAP;";
+
+$query = $conn->query($sql);
+$query->setFetchMode(PDO::FETCH_ASSOC);
+
+$stt = 0;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,93 +41,7 @@
         <div class="container w-75 bg-light">
 
             <!-- Header -->
-            <div class="d-flex justify-content-between align-items-center">
-
-                <!-- Search bar -->
-                <div class="input-group w-25">
-
-                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                        data-bs-auto-close="true" aria-expanded="false"></button>
-                    <ul class="dropdown-menu">
-                        <form class="p-1">
-
-                            <div class=""><strong>Giá</strong></div>
-                            <div class="d-flex justify-content-center">
-                                <input type="number" placeholder="Từ" class="w-50">
-                                <div class="">-</div>
-                                <input type="number" placeholder="Đến" class="w-50">
-                            </div>
-
-                            <div><strong>Tình trạng</strong></div>
-                            <div class="d-flex justify-content-around">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="Con" id="">
-                                    <label class="form-check-label">Còn</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="Het" id="">
-                                    <label class="form-check-label">Hết</label>
-                                </div>
-                            </div>
-                        </form>
-                    </ul>
-                    <input type="text" class="form-control" aria-label="Text input with dropdown button">
-
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon"><img
-                            src="../images/png/icons/001-loupe.png" alt=""></button>
-                </div>
-
-
-                <!-- Logo -->
-                <div class="">
-                    <a href="./home.php" class="h2 text-dark text-decoration-none">
-                        <img src="../images/svg/logo.svg" style="display: inline-block; width: 80px; height: auto;"
-                            alt="HCMUS"> HCMUS Canteen
-                    </a>
-                </div>
-
-
-                <!-- Thẻ này để đẩy logo ra giữa, ko để làm gì cả -->
-                <div class="w25"></div>
-
-
-                <!-- Profile -->
-                <div class="dropdown">
-                    <button class="btn btn-outline-info dropdown-toggle" type="button" id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="../images/png/icons/004-user.png" alt="">
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                        <li>
-                            <h3 class="dropdown-header text-end">Xin chào <span class="text-primary">Admin</span>
-                            </h3>
-                        </li>
-                        <li><a class="dropdown-item text-end" href="../index.php">Đăng xuất</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <h4 class="dropdown-header">Bạn là <span class="text-warning">Quản lý</span></h4>
-                        </li>
-                        <li><a class="dropdown-item" href="./profile.php">Thông tin cá nhân</a></li>
-                        <li><a class="dropdown-item" href="./password.php">Đổi mật khẩu</a></li>
-                        <li><a class="dropdown-item" href="./menu.php">Quản lý thực đơn</a></li>
-                        <li><a class="dropdown-item" href="./inventory.php">Quản lý hàng tồn</a></li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <!-- Image -->
-            <div class="row position-relative">
-                <img src="../images/svg/background2.svg" class="img p-0" style="filter: brightness(70%)" alt="">
-                <div class="text-white position-absolute" style="left: 25px; top: 10px;">
-                    <div class="h4">Cần 1 bữa ăn ngon bổ rẻ?</div>
-                    <div class="h5">Đừng lo</div>
-                    <div class="h5">Căn tin tự nhiên bao no</div>
-                </div>
-            </div>
+            <?php include "./header.php" ?>
 
             <!-- Main -->
             <div class="container" style="height:480px">
@@ -121,7 +52,7 @@
 
 
                 <!-- Body menu -->
-                <div class="row overflow-auto" style="width: 80%; margin: auto;">
+                <div class="row overflow-auto" style="width: 80%; height: 350px; margin: auto;">
                     <table class="table table-bordered table-hover text-center">
                         <thead>
                             <tr>
@@ -134,54 +65,16 @@
                             </tr>
                         </thead>
                         <tbody class="">
+                        <?php while ($row = $query->fetch()) { ?>
                             <tr>
-                                <td>1</td>
-                                <td>Trà xanh không độ</td>
-                                <td>19/11/2022</td>
-                                <td>2.000</td>
-                                <td>10.000</td>
-                                <td>Gò vấp</td>
+                                <td> <?php echo $stt; ++$stt;?> </td>
+                                <td> <?php echo $row['TENHANG']?> </td>
+                                <td> <?php echo $row['NGAYNHAP']?> </td>
+                                <td> <?php echo $row['SOLUONG']?> </td>
+                                <td> <?php echo $row['DONGIA']?> </td>
+                                <td> <?php echo $row['DIACHIKHO']?> </td>
                             </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>C2</td>
-                                <td>19/11/2022</td>
-                                <td>2.000</td>
-                                <td>10.000</td>
-                                <td>Q.10</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Nước suối</td>
-                                <td>20/11/2022</td>
-                                <td>2.000</td>
-                                <td>10.000</td>
-                                <td>Q.12</td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>Trà xanh không độ</td>
-                                <td>20/11/2022</td>
-                                <td>2.000</td>
-                                <td>10.000</td>
-                                <td>Q.10</td>
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>Nước suôi</td>
-                                <td>21/11/2022</td>
-                                <td>2.000</td>
-                                <td>10.000</td>
-                                <td>Q.9</td>
-                            </tr>
-                            <tr>
-                                <td>... </td>
-                                <td> </td>
-                                <td> </td>
-                                <td> </td>
-                                <td> </td>
-                                <td> </td>
-                            </tr>
+                        <?php } ?>
                         </tbody>
                     </table>
                 </div>
